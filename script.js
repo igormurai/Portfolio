bfd370fdaa98fd6aca2da48b8bc260ab0e2261c4
Igor Murai <125315090+igormurai@users.noreply.github.com>
2024-02-23 01:48:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui (se necessário)
+});
+
+function toggleModoClaroEscuro() {
+    const body = document.body;
+    body.classList.toggle('modo-escuro');
+}
